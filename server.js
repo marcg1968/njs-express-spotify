ebae84b603986124b36621300560c301177d628a
marc on red211 <mgreyling@gmail.com>
2023-07-27 16:39:28 +1000

    tw

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.use((req, res, next) => {
 app.get('/auth/login/:variant?', (req, res) => {
 
     console.log(78, { 'req.params': req.params })
-    let { variant } = req.params || {}
+    let { variant } = req.params
     variant = variant || 'PRODUCTION' /* default to PRODUCTION */
     variant = variant.replace(/[^0-9a-z_\-\+]/g, '')
     console.log(82, { variant })
